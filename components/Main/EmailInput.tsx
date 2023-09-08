4524ee62f6c32b51333926d2fc22125b33519ace
CJskii <czareczarek3@gmail.com>
2023-09-08 23:37:07 +0100

    Added email validation + user feedback

--- components/Main/EmailInput.tsx
+++ components/Main/EmailInput.tsx
@@ -1,10 +1,18 @@
-const EmailInput = (props: { setEmailAddress: (email: string) => void }) => {
-  const { setEmailAddress } = props;
+import { isValidEmail } from "../../utils/isValidEmailAddress";
+
+const EmailInput = (props: {
+  setEmailAddress: (email: string) => void;
+  emailAddress: string;
+}) => {
+  const { setEmailAddress, emailAddress } = props;
 
   return (
     <div className="form-control w-full max-w-[90%]">
       <label className="label">
         <span className="label-text">Email*</span>
+        {!isValidEmail(emailAddress) && emailAddress ? (
+          <span className="label-text text-red-600">Invalid email</span>
+        ) : null}
       </label>
       <input
         type="email"
