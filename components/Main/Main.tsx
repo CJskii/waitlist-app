715033cb784ec08cdacbae1ed878b9cb7bf94e7d
CJskii <czareczarek3@gmail.com>
2023-09-09 00:21:27 +0100

    added motion + close icon

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,32 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Logo from "./Logo";
 import EmailInput from "./EmailInput";
 import WalletInput from "./WalletInput";
 import JoinButton from "../Buttons/JoinButton";
 import TermsConditions from "./Terms&Conditions";
 import Header from "./Header";
-import { isValidEmail } from "../../utils/isValidEmailAddress";
-import { isValidEthereumAddress } from "../../utils/isValidEthereumAddress";
 
 const Main = () => {
   const [emailAddress, setEmailAddress] = useState("");
   const [walletAddress, setWalletAddress] = useState("");
-
-  useEffect(() => {
-    if (emailAddress && !isValidEmail(emailAddress)) {
-      // Handle invalid email format here, perhaps show an error message to the user
-      console.log("Invalid email format");
-    }
-
-    if (walletAddress && !isValidEthereumAddress(walletAddress)) {
-      // Handle invalid Ethereum address format here, perhaps show an error message to the user
-      console.log("Invalid Ethereum address format");
-    }
-  }, [emailAddress, walletAddress]);
-
-  // pass is connected to the button
-  // pass email address to the button
-  // pass wallet address to the button
 
   return (
     <div className="flex flex-col justify-betweeen items-center min-w-full">
@@ -34,7 +16,6 @@ const Main = () => {
         <div className="flex flex-col justify-start items-center gap-4 bg-base max-w-[500px]">
           <Logo />
           <div className="w-full flex flex-col justify-center items-center gap-4">
-            {/* Mint Form */}
             <Header />
             <EmailInput
               setEmailAddress={setEmailAddress}
--- components/Toast.tsx
+++ components/Toast.tsx
@@ -1,14 +1,29 @@
+import { motion } from "framer-motion";
+import { IoMdCloseCircleOutline } from "react-icons/io";
+
 const Toast: React.FC<{ message: string; onClose: () => void }> = ({
   message,
   onClose,
 }) => {
+  const variants = {
+    hidden: { opacity: 0, x: 100 },
+    visible: { opacity: 1, x: 0 },
+  };
+
   return (
-    <div className="fixed right-0 top-[10%] mt-4 mr-4 p-4 bg-error text-error-content rounded-lg shadow-md z-50">
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+      variants={variants}
+      className="fixed right-0 top-[15%] mt-4 mr-4 p-4 bg-error text-error-content rounded-lg shadow-md z-50 flex justify-center items-center"
+    >
       {message}
+
       <button onClick={onClose} className="ml-4">
-        x
+        <IoMdCloseCircleOutline />
       </button>
-    </div>
+    </motion.div>
   );
 };
 
