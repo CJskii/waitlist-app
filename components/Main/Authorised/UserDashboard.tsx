ecb978656206add953a640868d58c07ca95a1d53
CJskii <czareczarek3@gmail.com>
2023-09-10 21:27:06 +0100

    fetch user data API

--- components/Main/Authorised/UserDashboard.tsx
+++ components/Main/Authorised/UserDashboard.tsx
@@ -3,6 +3,7 @@ import UserStats from "./UserStats";
 import ButtonGroup from "../../Buttons/ButtonGroup";
 import CopyReflink from "./CopyReflink";
 import ConnectWithUs from "./ConnectWithUs";
+import { useEffect, useState } from "react";
 
 interface UserDashboardProps {
   userDashboardProps: {
@@ -13,6 +14,45 @@ interface UserDashboardProps {
 
 const UserDashboard = (props: UserDashboardProps) => {
   const { walletAddress, setJoined } = props.userDashboardProps;
+  const [userData, setUserData] = useState<any | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(`/api/getUserDetails`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ ethereumAddress: walletAddress }),
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+
+        const data = await response.json();
+        setUserData(data.user);
+        setIsLoading(false);
+      } catch (err) {
+        setError((err as any).message || "An error occurred.");
+        setIsLoading(false);
+      }
+    };
+
+    fetchUserData();
+  }, [walletAddress]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex px-2 justify-center items-center w-full">
@@ -24,7 +64,7 @@ const UserDashboard = (props: UserDashboardProps) => {
         </h1>
       </div>
 
-      <UserStats />
+      <UserStats data={userData ? userData : {}} />
 
       <h1 className="w-full px-4 text-xl font-bold italic">
         Want to get ahead in line?
@@ -37,7 +77,9 @@ const UserDashboard = (props: UserDashboardProps) => {
       </span>
       <ButtonGroup />
 
-      <CopyReflink />
+      <CopyReflink
+        inviteLink={userData.inviteLink ? userData.inviteLink : ""}
+      />
 
       <ConnectWithUs />
     </div>
