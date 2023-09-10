b7acc7f21ccb5a1f6ffd12bcb4c009893e77a783
CJskii <czareczarek3@gmail.com>
2023-09-10 21:27:18 +0100

    populate fields with user data

--- components/Main/Authorised/CopyReflink.tsx
+++ components/Main/Authorised/CopyReflink.tsx
@@ -1,10 +1,14 @@
-const CopyReflink = () => {
+const CopyReflink = (props: { inviteLink: string }) => {
+  const { inviteLink } = props;
+
   return (
     <div className="flex justify-start gap-4 items-center w-full px-4 py-2">
       <input
         type="text"
         placeholder="Type here"
         className="input w-full max-w-xs"
+        defaultValue={`https://www.mintly.lol/?invite=${inviteLink}`}
+        readOnly
       />
       <button className="btn bg-base-300 text-base-content text-xl rounded-lg p-2 px-8 border-2 border-transparent hover:border-accent">
         Copy
--- components/Main/Authorised/UserStats.tsx
+++ components/Main/Authorised/UserStats.tsx
@@ -1,4 +1,15 @@
-const UserStats = () => {
+interface UserStatsProps {
+  data: {
+    ethereumAddress: string;
+    leaderboardPosition: number;
+    totalPoints: number;
+    inviteLink: number;
+    inviteCount: number;
+  };
+}
+
+const UserStats = (props: UserStatsProps) => {
+  const { leaderboardPosition, totalPoints, inviteCount } = props.data;
   return (
     <div className="grid grid-cols-3 stats my-4 bg-transparent w-full ">
       <div className="stat">
@@ -6,7 +17,7 @@ const UserStats = () => {
           Position
         </div>
         <div className="stat-value w-full flex justify-center items-center">
-          31768
+          {leaderboardPosition ? leaderboardPosition : "N/A"}
         </div>
       </div>
 
@@ -14,14 +25,18 @@ const UserStats = () => {
         <div className="stat-title flex justify-center items-center">
           XP points
         </div>
-        <div className="stat-value flex justify-center items-center">4200</div>
+        <div className="stat-value flex justify-center items-center">
+          {totalPoints ? totalPoints : "N/A"}
+        </div>
       </div>
 
       <div className="stat">
         <div className="stat-title flex justify-center items-center">
           Invited friends
         </div>
-        <div className="stat-value flex justify-center items-center">55</div>
+        <div className="stat-value flex justify-center items-center">
+          {inviteCount ? inviteCount : "N/A"}
+        </div>
       </div>
     </div>
   );
